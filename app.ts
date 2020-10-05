ca665b90862d9a7d8591684052624c18ebc52b7f
Eldar Khaitov <Eldar.Khaitov@mheducation.com>
2020-10-05 18:50:53 +0300

    Refactored to remove unnecessary libs

--- app.ts
+++ app.ts
@@ -1,13 +1,10 @@
-import server from './server';
+import * as express from 'express';
 import * as dotenv from 'dotenv';
-import * as mongoose from "mongoose";
-import {Logger} from "@overnightjs/logger";
 
 dotenv.config();
 
-const srv = new server();
+const app = express();
 
-mongoose.connect(process.env.MONGODB_URI as string, {useNewUrlParser: true, useUnifiedTopology: true})
-  .then(Logger.Info);
-
-srv.start(process.env.PORT as any);
+app.listen(process.env.PORT, () => {
+    console.log('listening on port', process.env.PORT);
+});
