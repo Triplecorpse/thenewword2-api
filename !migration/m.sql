c7aa038603fc00c087b73ba10c2ce7f5c76e6628
Eldar Khaitov <Eldar.Khaitov@mheducation.com>
2020-10-06 19:01:53 +0300

    Added password column

--- !migration/m.sql
+++ !migration/m.sql
@@ -2,6 +2,7 @@ CREATE SCHEMA IF NOT EXISTS tnw2 AUTHORIZATION postgres;
 CREATE TABLE IF NOT EXISTS tnw2.users (
     id serial PRIMARY KEY,
     login text NOT NULL UNIQUE CHECK(login != ''),
+    password text NOT NULL,
     email text NOT NULL UNIQUE CHECK(email != '')
 );
 CREATE TABLE IF NOT EXISTS tnw2.words (
