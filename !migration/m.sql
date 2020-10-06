79dabf82ef00a4ad973501fd459424122833a1b7
Eldar Khaitov <Eldar.Khaitov@mheducation.com>
2020-10-06 18:35:02 +0300

    Creating db init script

--- !migration/m.sql
+++ !migration/m.sql
@@ -1,12 +1,11 @@
 CREATE SCHEMA IF NOT EXISTS tnw2 AUTHORIZATION postgres;
-CREATE TABLE IF NOT EXISTS tnw2.tnw2_users (
+CREATE TABLE IF NOT EXISTS tnw2.users (
     id serial PRIMARY KEY,
     login text NOT NULL UNIQUE CHECK(login != ''),
     email text NOT NULL UNIQUE CHECK(email != '')
 );
-CREATE TABLE IF NOT EXISTS tnw2.tnw2_words (
+CREATE TABLE IF NOT EXISTS tnw2.words (
     id serial PRIMARY KEY,
-    PRIMARY KEY(id),
     word text NOT NULL UNIQUE CHECK(word != ''),
     translations text[],
     speechPart text,
@@ -15,7 +14,10 @@ CREATE TABLE IF NOT EXISTS tnw2.tnw2_words (
     original_language char(2),
     translated_language char(2),
     remarks text,
-    user_created integer NOT NULL REFERENCES tnw2.tnw_users(id),
-    users_copied integer[] NOT NULL REFERENCES tnw2.tnw_users(id),
+    user_created integer REFERENCES tnw2.users(id),
     stress_letter_index smallint
+);
+CREATE TABLE IF NOT EXISTS tnw2.relation_words_users_copied (
+    userId integer NOT NULL REFERENCES tnw2.users(id),
+    wordId integer NOT NULL REFERENCES tnw2.words(id)
 );
