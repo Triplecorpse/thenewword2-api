9df78192c743d761b07a325ded537d254e9f47a8
Eldar Khaitov <Eldar.Khaitov@mheducation.com>
2020-10-15 17:39:59 +0300

    made more beautiful tester html

--- app.ts
+++ app.ts
@@ -29,7 +29,7 @@ connectToDatabase()
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use((req: Request, res: Response, next: NextFunction) => {
-    if (process.env.MODE === 'development') {
+    if (process.env.MODE === 'DEVELOPMENT') {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', '*');
         res.setHeader('Access-Control-Allow-Headers', '*');
